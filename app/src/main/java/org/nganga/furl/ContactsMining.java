d94ad4e5a558a4ba101ec369d51f51d02c689e9d
martnga <mtnjoroge@gmail.com>
2015-10-14 18:05:55 +0300

    Loading contacts to a list

--- app/src/main/java/org/nganga/furl/ContactsMining.java
+++ app/src/main/java/org/nganga/furl/ContactsMining.java
@@ -1,29 +1,37 @@
 package org.nganga.furl;
 
-import android.app.Activity;
+
+import android.app.ListActivity;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.support.v4.widget.SimpleCursorAdapter;
 import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
+import android.widget.ListView;
 
 /**
  * Created by mansa on 10/8/15.
  */
-public class ContactsMining extends Activity {
+public class ContactsMining extends ListActivity {
+
+    ListView contactList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.contacts);
 
-       /* Cursor contacts = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
-        while (contacts.moveToNext()){
-            int contactsIndex = contacts.getColumnIndex(ContactsContract.PhoneLookup.NUMBER);
-            String numbers = contacts.getString(contactsIndex);
-            Log.d("Numbers", numbers);
-        }*/
+        Cursor contacts = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
+        startManagingCursor(contacts);
+            String[] from = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone._ID};
+            int[] to = {android.R.id.text1, android.R.id.text2};
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, contacts,from,to);
+            setListAdapter(listAdapter);
+            contactList = getListView();
+            contactList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+
 
         setUpViews();
     }

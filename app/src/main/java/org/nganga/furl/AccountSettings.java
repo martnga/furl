650d82af464d3be7a83d4598feffa7702f987510
martnga <mtnjoroge@gmail.com>
2015-10-14 06:37:36 +0300

    Rid Unused files

--- app/src/main/java/org/nganga/furl/AccountSettings.java
+++ app/src/main/java/org/nganga/furl/AccountSettings.java
@@ -10,26 +10,37 @@ import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
+
+import com.digits.sdk.android.Digits;
+import com.twitter.sdk.android.Twitter;
 
 
 public class AccountSettings extends Activity {
 
     private ViewPager viewPager;
-
+    private Button logOutBtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
-
         setUpViews();
+        logOutBtn = (Button) findViewById(R.id.logOutBtn);
+        logOutBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+               deleteSession();
+            }
+        });
+
     }
 
     private void setUpViews() {
-        setUpCount();
         setUpBack();
 
     }
+
 
     private void setUpBack() {
         // go back if clicked
@@ -52,5 +63,13 @@ public class AccountSettings extends Activity {
             }
         });
     }
+
+
+    public void deleteSession(){
+
+        Digits.getSessionManager().clearActiveSession();
+        Twitter.getSessionManager().clearActiveSession();
+    }
+
 
 }

21740fdea0c468a083fc4513d401233a4c7495d2
martnga <mtnjoroge@gmail.com>
2015-11-08 12:20:41 +0300

    Refined contacts click

--- app/src/main/java/org/nganga/furl/ContactsMining.java
+++ app/src/main/java/org/nganga/furl/ContactsMining.java
@@ -2,19 +2,20 @@ package org.nganga.furl;
 
 
 import android.app.ListActivity;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.support.v4.widget.SimpleCursorAdapter;
-import android.util.Log;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.ListView;
 
+
 /**
  * Created by mansa on 10/8/15.
  */
-public class ContactsMining extends ListActivity {
+public class ContactsMining extends ListActivity  {
 
     ListView contactList;
 
@@ -26,15 +27,36 @@ public class ContactsMining extends ListActivity {
         Cursor contacts = getContentResolver().query(ContactsContract.Contacts.CONTENT_URI, null, null, null, null);
         startManagingCursor(contacts);
             String[] from = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME, ContactsContract.CommonDataKinds.Phone._ID};
-            int[] to = {android.R.id.text1, android.R.id.text2};
-            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this, android.R.layout.simple_list_item_2, contacts,from,to);
-            setListAdapter(listAdapter);
-            contactList = getListView();
-            contactList.setChoiceMode(ListView.CHOICE_MODE_MULTIPLE);
+            int[] to = {R.id.phone_name, R.id.phone_number};
+            SimpleCursorAdapter listAdapter = new SimpleCursorAdapter(this, R.layout.contact_row, contacts,from,to);
+        contactList = (ListView)findViewById(R.id.contact_list);
+        contactList.setAdapter(listAdapter);
+
 
 
         setUpViews();
     }
+
+    @Override
+    protected void onListItemClick(ListView l, View v, int position, long id) {
+        // TODO Auto-generated method stub
+        super.onListItemClick(l, v, position, id);
+
+
+        Cursor cursor = ((SimpleCursorAdapter)l.getAdapter()).getCursor();
+        cursor.moveToPosition(position);
+
+        String phone_number = cursor.getString(cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone._ID));
+
+        Intent bucket = new Intent(this, Map.class);
+
+        Bundle b = new Bundle();
+        b.putString("phonenumber", phone_number);
+        bucket.putExtras(b);
+        startActivity(bucket);
+
+    }
+
 
     private void setUpViews() {
         setUpCount();
---
+++ app/src/main/java/org/nganga/furl/Map.java
@@ -0,0 +1,16 @@
+package org.nganga.furl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+/**
+ * Created by mansa on 11/8/15.
+ */
+public class Map extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.map);
+    }
+}

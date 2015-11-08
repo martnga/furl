266c8499b8331097ff97d8632cd29f98f81a01b3
martnga <mtnjoroge@gmail.com>
2015-11-08 16:46:04 +0300

    Get rid of recyclerview

--- app/src/main/java/org/nganga/furl/Strangers.java
+++ app/src/main/java/org/nganga/furl/Strangers.java
@@ -6,7 +6,6 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -29,8 +28,6 @@ import java.util.List;
 
 
 public class Strangers extends Activity{
-    RecyclerView strangers_list;
-    StrangersRecyclerAdapter adapter;
 
     /** The Chat list. */
     private ArrayList<ParseUser> uList;
